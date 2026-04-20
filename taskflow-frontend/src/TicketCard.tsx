e9c40ac1f57f58defc8893e8945d095ac521cfae
NFAsylum <marcooinotna13@outlook.com>
2026-04-20 01:07:55 -0300

    Add ticket edit; Fix scripts formatting

--- taskflow-frontend/src/TicketCard.tsx
+++ taskflow-frontend/src/TicketCard.tsx
@@ -1,21 +1,104 @@
-import { Priority, type TicketCardProps } from './types'
+import { useState } from 'react'
+import { Priority, Status, type TicketCardProps } from './types'
 
 const priorityStyle = {
-    [Priority.Low]: "bg-green-500 rounded text-sm p-2",
-    [Priority.Medium]: "bg-yellow-500 rounded text-sm p-2",
-    [Priority.High]: "bg-red-500 rounded text-sm p-2"
+  [Priority.Low]: 'bg-green-500 rounded text-sm p-2',
+  [Priority.Medium]: 'bg-yellow-500 rounded text-sm p-2',
+  [Priority.High]: 'bg-red-500 rounded text-sm p-2',
 }
 
-function TicketCard({ id, title, priority, onMove, onDelete }: TicketCardProps){
-    return(
-        <div className="bg-yellow-100 p-5 rounded-lg border border-amber-400">
-            <button className="bg-red-500 p-1 rounded-2xl" onClick={onDelete}>X</button>
-            <h3 className="text-gray-800">{id}</h3>
-            <p className="text-gray-800 p-2">{title}</p>
-            <span className={priorityStyle[priority]}>{priority}</span>
-            <button className="bg-gray-400" onClick={onMove}>{"<>"}</button>
-        </div>
-    )
+function TicketCard({
+  id,
+  title,
+  priority,
+  status,
+  onMove,
+  onDelete,
+  onEdit,
+}: TicketCardProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const [editTitle, setEditTitle] = useState<string>(title)
+  const [editPriority, setEditPriority] = useState<Priority>(priority)
+  const [editStatus, setEditStatus] = useState<Status>(status)
+
+  return (
+    <div className="bg-yellow-100 p-5 rounded-lg border border-amber-400">
+      <button className="bg-red-500 p-1 rounded-2xl" onClick={onDelete}>
+        X
+      </button>
+      {isEditing ? (
+        <>
+          <div>
+            <label className="font-bold pr-4">Task title</label>
+            <input
+              className="bg-gray-200 rounded-4xl"
+              type="text"
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+              placeholder="Title"
+            />
+          </div>
+
+          <div>
+            <label className="font-bold pr-4">Priority</label>
+            <select
+              className="bg-gray-200 rounded-4xl"
+              value={editPriority}
+              onChange={(e) => setEditPriority(e.target.value as Priority)}
+            >
+              {Object.values(Priority).map((priority) => (
+                <option key={priority} value={priority}>
+                  {priority}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label className="font-bold pr-4">Status</label>
+            <select
+              className="bg-gray-200 rounded-4xl"
+              value={editStatus}
+              onChange={(e) => setEditStatus(e.target.value as Status)}
+            >
+              {Object.values(Status).map((status) => (
+                <option key={status} value={status}>
+                  {status}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            className="bg-purple-300 p-1 rounded-2xl"
+            onClick={() => {
+              onEdit(editTitle, editPriority, editStatus)
+              setIsEditing(false)
+            }}
+          >
+            Save
+          </button>
+        </>
+      ) : (
+        <>
+          <p className="text-gray-800 p-2">{title}</p>
+          <span className={priorityStyle[priority]}>{priority}</span>
+          <button
+            className="bg-purple-300 p-1 rounded-2xl"
+            onClick={() => {
+              setIsEditing(true)
+            }}
+          >
+            Edit
+          </button>
+        </>
+      )}
+      <h3 className="text-gray-800">{id}</h3>
+      <button className="bg-gray-400" onClick={onMove}>
+        {'<>'}
+      </button>
+    </div>
+  )
 }
 
 export default TicketCard
--- taskflow-frontend/src/TicketForm.tsx
+++ taskflow-frontend/src/TicketForm.tsx
@@ -1,61 +1,74 @@
 import { useState } from 'react'
-import {Priority, Status } from './types'
+import { Priority, Status } from './types'
 
-interface TicketFormProps{
-    onCreate: (title: string, priority: Priority, status: Status) => void
+interface TicketFormProps {
+  onCreate: (title: string, priority: Priority, status: Status) => void
 }
 
-function TicketForm({ onCreate }: TicketFormProps){
-    const [title, setTitle] = useState<string>("Task")
-    const [priority, setPriority] = useState<Priority>(Priority.Low)
-    const [status, setStatus] = useState<Status>(Status.Open)
+function TicketForm({ onCreate }: TicketFormProps) {
+  const [title, setTitle] = useState<string>('Task')
+  const [priority, setPriority] = useState<Priority>(Priority.Low)
+  const [status, setStatus] = useState<Status>(Status.Open)
 
-    function handleSubmit(submitEvent: SubmitEvent){
-        submitEvent.preventDefault();
-        onCreate(title, priority, status)
+  function handleSubmit(submitEvent: SubmitEvent) {
+    submitEvent.preventDefault()
+    onCreate(title, priority, status)
 
-        setTitle("Task")
-        setPriority(Priority.Low)
-        setStatus(Status.Open)
-    }
+    setTitle('Task')
+    setPriority(Priority.Low)
+    setStatus(Status.Open)
+  }
 
-    return(
-        <div className="m-2 p-4 border-4 rounded-2xl bg-blue-100">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label className="font-bold pr-4">Task title</label>
-                    <input className="bg-gray-200 rounded-4xl" type="text" value={title}
-                    onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
-                </div>
-
-                <div>
-                    <label className="font-bold pr-4">Priority</label>
-                    <select className="bg-gray-200 rounded-4xl" value={priority}
-                    onChange={(e) => setPriority(e.target.value as Priority)} >
-                        {Object.values(Priority).map((priority) => (
-                            <option
-                            key={priority}
-                            value={priority}>{priority}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <div>
-                    <label className="font-bold pr-4">Status</label>
-                    <select className="bg-gray-200 rounded-4xl" value={status}
-                    onChange={(e) => setStatus(e.target.value as Status)} >
-                        {Object.values(Status).map((status) => (
-                            <option
-                            key={status}
-                            value={status}>{status}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <button className="bg-gray-400 m-2 p-3 rounded-2xl" type="submit">Submit</button>
-            </form>
+  return (
+    <div className="m-2 p-4 border-4 rounded-2xl bg-blue-100">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label className="font-bold pr-4">Task title</label>
+          <input
+            className="bg-gray-200 rounded-4xl"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+          />
         </div>
-    )
+
+        <div>
+          <label className="font-bold pr-4">Priority</label>
+          <select
+            className="bg-gray-200 rounded-4xl"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as Priority)}
+          >
+            {Object.values(Priority).map((priority) => (
+              <option key={priority} value={priority}>
+                {priority}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label className="font-bold pr-4">Status</label>
+          <select
+            className="bg-gray-200 rounded-4xl"
+            value={status}
+            onChange={(e) => setStatus(e.target.value as Status)}
+          >
+            {Object.values(Status).map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button className="bg-gray-400 m-2 p-3 rounded-2xl" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default TicketForm
